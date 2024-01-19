d13cf0be4538f266f91ce72ab618823ac10f4667
Igor Kedzierawski <igor.kedzierawski@mail.pl>
2024-01-19 13:44:03 +0100

    removed unused import

--- corn-backend/src/main/java/dev/corn/cornbackend/api/sprint/SprintServiceImpl.java
+++ corn-backend/src/main/java/dev/corn/cornbackend/api/sprint/SprintServiceImpl.java
@@ -17,7 +17,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
