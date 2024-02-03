51a6595726e2ff0c8ad995ac5a37a3efcaa92a7e
Szymon Półtorak <szymonpotorak@gmail.com>
2024-02-03 15:39:39 +0100

    Added toolbar

--- corn-frontend/src/app/pages/home/home.component.ts
+++ corn-frontend/src/app/pages/home/home.component.ts
@@ -1,19 +1,18 @@
 import { Component } from '@angular/core';
 import { MatCard, MatCardHeader } from "@angular/material/card";
-import { MatButton } from "@angular/material/button";
+import { MatButton, MatIconButton } from "@angular/material/button";
 import { MatFormField, MatHint } from "@angular/material/form-field";
 import { MatInput } from "@angular/material/input";
+import { MatToolbar } from "@angular/material/toolbar";
+import { MatIcon } from "@angular/material/icon";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     imports: [
-        MatCard,
-        MatCardHeader,
-        MatButton,
-        MatFormField,
-        MatInput,
-        MatHint
+        MatToolbar,
+        MatIcon,
+        MatButton
     ],
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss'
