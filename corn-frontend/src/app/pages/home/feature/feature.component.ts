b0868aefaed1eab62381b4fd7efa01ec1b87ed8c
Szymon Półtorak <szymonpotorak@gmail.com>
2024-02-03 17:45:32 +0100

    Refactored a bit and added new few things

--- corn-frontend/src/app/pages/home/feature/feature.component.ts
+++ corn-frontend/src/app/pages/home/feature/feature.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatTab } from "@angular/material/tabs";
+import { Feature } from "@core/interfaces/home/feature.interface";
 
 @Component({
-  selector: 'app-feature',
-  standalone: true,
-  imports: [],
-  templateUrl: './feature.component.html',
-  styleUrl: './feature.component.scss'
+    selector: 'app-feature',
+    standalone: true,
+    imports: [
+        MatTab
+    ],
+    templateUrl: './feature.component.html',
+    styleUrl: './feature.component.scss'
 })
 export class FeatureComponent {
-
+    @Input() feature !: Feature;
 }
--- corn-frontend/src/app/pages/home/home.component.ts
+++ corn-frontend/src/app/pages/home/home.component.ts
@@ -3,6 +3,8 @@ import { MatButton } from "@angular/material/button";
 import { MatToolbar } from "@angular/material/toolbar";
 import { MatIcon } from "@angular/material/icon";
 import { MatTab, MatTabGroup } from "@angular/material/tabs";
+import { FeatureComponent } from "@pages/home/feature/feature.component";
+import { Feature } from "@core/interfaces/home/feature.interface";
 
 @Component({
     selector: 'app-home',
@@ -12,11 +14,37 @@ import { MatTab, MatTabGroup } from "@angular/material/tabs";
         MatIcon,
         MatButton,
         MatTabGroup,
-        MatTab
+        MatTab,
+        FeatureComponent
     ],
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+    protected readonly features: Feature[] = [
+        {
+            content: "We offer easy way to manage your different projects. Each project is one place allowing you to switch between them easily.",
+            isLeft: false,
+            label: "Manage Projects",
+            title: "All projects in one place"
+        },
+        {
+            content: "You can easily plan new sprints and manage them. Everything is very simple.",
+            isLeft: true,
+            label: "Manage Sprints",
+            title: "One view to rule them all"
+        },
+        {
+            content: "Add, remove and edit tasks in appropriate sprint. Manage descriptions, add comments and assign them to users.",
+            isLeft: false,
+            label: "Work with Tasks",
+            title: "Easy to add, easy to remove"
+        },
+        {
+            content: "Manage your backlog entries to easily deal with work to do. Easy view for whole work.",
+            isLeft: true,
+            label: "Deal with Backlog",
+            title: "All tasks in one backlog"
+        }
+    ];
 }
