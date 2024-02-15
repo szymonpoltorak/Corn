04b0b7bf495c46785fa6f4654ee2e6184cfe5b8a
Igor Kedzierawski <igor.kedzierawski@mail.pl>
2024-02-15 19:24:42 +0100

    cleaned up some code

--- corn-frontend/src/app/pages/home/home.component.ts
+++ corn-frontend/src/app/pages/home/home.component.ts
@@ -27,30 +27,30 @@ import { UserinfoComponent } from './userinfo/userinfo.component';
     styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-    
-    public isLoggedIn = false;
-    public userProfile: KeycloakProfile | null = null;
+
+    isLoggedIn: boolean = false;
+    userProfile?: KeycloakProfile;
 
     constructor(
         readonly keycloak: KeycloakService
     ) { }
 
-    public async ngOnInit() {
-        this.isLoggedIn = await this.keycloak.isLoggedIn();
+    async ngOnInit() {
+        this.isLoggedIn = this.keycloak.isLoggedIn();
         if (this.isLoggedIn) {
             this.userProfile = await this.keycloak.loadUserProfile();
         }
     }
 
-    public login() {
+    login() {
         this.keycloak.login();
     }
 
-    public logout() {
+    logout() {
         this.keycloak.logout();
     }
 
-    public register() {
+    register() {
         this.keycloak.register();
     }
 
