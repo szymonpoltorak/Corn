9d97952477c502c88dec03b1cde0b3c2cd613d59
Higunio320 <null>
2024-03-18 17:32:36 +0100

    Changed path to value from enum

--- corn-frontend/src/app/core/enum/RouterPaths.ts
+++ corn-frontend/src/app/core/enum/RouterPaths.ts
@@ -5,6 +5,9 @@ export enum RouterPaths {
     HOME_DIRECT_PATH = "/home",
     
     BOARDS_PATH = "boards",
+
     PROJECT_LIST_PATH = "projects",
+    PROJECT_LIST_DIRECT_PATH = "/projects",
+
     UNKNOWN_PATH = "**"
 }
